0c56a73a41e3eaebcd694058d875ff46740c914f
matjazonline <matjazonline@gmail.com>
2017-12-01 16:50:46 +0100

    init

---
+++ adaptr.go
@@ -0,0 +1,54 @@
+package adaptr
+
+import (
+	"net/http"
+	"context"
+	"sync"
+	)
+
+type Adapter func(handle http.Handler) http.Handler
+
+func Adapt(h http.Handler, adapters ...Adapter) http.Handler {
+	/*for i := len(adapters); i >= 0; i-- {
+		h = adapters[i](h)
+	}*/
+	for i := len(adapters) - 1; i >= 0; i-- {
+		h = adapters[i](h)
+	}
+	return h
+}
+
+
+func SetCtxValue(r *http.Request, key, value interface{}) (*http.Request) {
+	return r.WithContext(context.WithValue(r.Context(), key, value))
+}
+
+func GetCtxValue(r *http.Request, key interface{}) interface{} {
+	return r.Context().Value(key)
+}
+
+func GetCtxValueStr(r *http.Request, key interface{}) string {
+	return GetCtxValue(r, key).(string)
+}
+
+func CallOnce(f func(w http.ResponseWriter, r *http.Request)) Adapter {
+	once:=sync.Once{}
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			//println("CallOnce called")
+			once.Do(func() {
+				f(w, r)
+			})
+			h.ServeHTTP(w, r)
+		})
+	}
+}
+
+func JsonContentType() Adapter {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			h.ServeHTTP(w, r)
+		})
+	}
+}
---
+++ httprouter.go
@@ -0,0 +1,42 @@
+package adaptr
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"sync"
+)
+
+var once = sync.Once{}
+
+// Simple accepts the name of a function so you don't have to wrap it with http.HandlerFunc
+// Example: r.GET("/", httprouterwrapper.Simple(controller.Index))
+func compatibleHandlerFn(h http.HandlerFunc, httprParamsCtxKey interface{}) httprouter.Handle {
+	return toHttpRouterHandle(http.Handler(h), httprParamsCtxKey )
+}
+
+// Compatible accepts a handler to make it compatible with http.HandlerFunc
+// Example: r.GET("/", httprouterwrapper.Compatible(http.HandlerFunc(controller.Index)))
+func compatibleHandler(h http.Handler, httprParamsCtxKey interface{}) httprouter.Handle {
+	return toHttpRouterHandle(h, httprParamsCtxKey)
+}
+func toHttpRouterHandle(h http.Handler, httprParamsCtxKey interface{}) func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		if httprParamsCtxKey != nil {
+			r = SetCtxValue(r, httprParamsCtxKey, p)
+		}
+		h.ServeHTTP(w, r)
+	}
+}
+
+func HttprouterAdaptFn(f http.HandlerFunc, httprParamsCtxKey interface{}, adapters ...Adapter) httprouter.Handle {
+	return HttprouterAdapt(http.HandlerFunc(f), httprParamsCtxKey, adapters...)
+}
+func HttprouterAdapt(h http.Handler, httprParamsCtxKey interface{}, adapters ...Adapter) httprouter.Handle {
+
+	///h = authBouncer()(h)
+	/*for i := len(adapters) - 1; i >= 0; i-- {
+		h = adapters[i](h)
+	}*/
+	h = Adapt(h, adapters...)
+	return compatibleHandler(h, httprParamsCtxKey)
+}
